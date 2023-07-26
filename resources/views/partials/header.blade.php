5854a1f8604ad59097089449e5adcfb5d15eb465
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-07-26 16:12:49 +0200

    non prende le immagini

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="header-top">
+
+    </div>
+    <div class="header-bottom">
+        <div class="container">
+            <div class="navbar">
+                <div class="logo">
+                    <a href="{{('/')}}}">
+                        <img src="" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>

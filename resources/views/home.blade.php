817de5be000640ea6db6ff5ecec1cd16b2970986
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-07-26 18:12:51 +0200

    file importato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
 </head>
 
 <body>
-
+   @include('partials.header')
     <main class="bg-light">
        dc comics
     </main>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,10 +6,47 @@
         <div class="container">
             <div class="navbar">
                 <div class="logo">
-                    <a href="{{('/')}}}">
-                        <img src="" alt="">
+                    <a href="{{ url('/') }}">
+                        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="dc-logo">
                     </a>
                 </div>
+                <nav >
+                    <ul class="list-unstyled">
+                        <li class="list-item">
+                            <a href="#" class="{{Route::currentRouteName() === 'homapge' ? 'active' : '' }}">Characters</a>
+                        </li>
+                        <li class="list-item">
+                            <a href="#">comics</a>
+                        </li>
+                        <li class="list-item">
+                            <a href="#">Movie</a>
+                        </li>
+                        <li class="list-item">
+                            <a href="#">tv</a>
+                        </li>
+                        <li class="list-item">
+                            <a href="#">games</a>
+                        </li>
+                        <li class="list-item">
+                            <a href="#">Collectibles</a>
+                        </li>
+                        <li class="list-item">
+                            <a href="#">videos</a>
+                        </li>
+                        <li class="list-item">
+                            <a href="#">fans</a>
+                        </li>
+                        <li class="list-item">
+                            <a href="#">news</a>
+                        </li>
+                        <li class="list-item">
+                            <a href="#">shop</a>
+                        </li>  
+                    </ul>             
+                </nav> 
+                 <div class="search">
+                    <input type="text" placeholder="Search">
+                 </div>         
             </div>
         </div>
     </div>
